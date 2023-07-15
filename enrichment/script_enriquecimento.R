61d198089bac7c299623eeb4d20e08c5e16f5918
resendejss <jean.souza@edu.unipar.br>
2023-07-15 16:07:44 -0300

    atualizacao

--- enrichment/script_enriquecimento.R
+++ enrichment/script_enriquecimento.R
@@ -737,3 +737,102 @@ browseKEGG(kk, 'dre00980')
 browseKEGG(kk, 'dre00983')
 browseKEGG(kk, 'dre00500')
 
+# -- CTcut5_CTcut0 all samples-- ###########################################
+data.genes <- read.csv("../differential_expression/allSamples/CTcut5_CTcut0_res05_sig_fc0.csv")
+
+genes <- data.genes[data.genes$log2FoldChange > 0.5 | data.genes$log2FoldChange < -0.5,]
+genes <- genes[!is.na(genes$padj),]
+genes <- genes[genes$padj < 0.05,]
+genes <- genes$X
+idx <- match(genes,gene_id$ensembl_gene_id)
+entrez <- gene_id$entrezgene_id[idx]
+entrez <- entrez[!is.na(entrez)]
+entrez <- as.character(entrez)
+
+ggo <- groupGO(gene = entrez,
+               OrgDb = org.Dr.eg.db,
+               ont = "CC",
+               #level = 3,
+               readable = T)
+
+kk <- enrichGO(gene = entrez,
+               OrgDb = org.Dr.eg.db,
+               pAdjustMethod = "BH",
+               ont="BP",
+               readable = T)
+
+kk <- enrichGO(gene = entrez,
+               OrgDb = org.Dr.eg.db,
+               pAdjustMethod = "BH",
+               ont="MF",
+               readable = T)
+
+kk <- enrichGO(gene = entrez,
+               OrgDb = org.Dr.eg.db,
+               pAdjustMethod = "BH",
+               ont="CC",
+               readable = T)
+
+head(kk)
+barplot(kk)
+goplot(kk)
+
+# -- KEEG
+kk <- enrichKEGG(gene = entrez,
+                 organism = "dre",
+                 pvalueCutoff = 0.05)
+
+head(kk)
+kk$ID
+
+browseKEGG(kk, 'dre04146')
+
+# -- CTcut5_CTcut0 filt samples-- ###########################################
+data.genes <- read.csv("../differential_expression/filtSamples/CTcut5_CTcut0_res05_sig_fc0.csv")
+
+genes <- data.genes[data.genes$log2FoldChange > 0.5 | data.genes$log2FoldChange < -0.5,]
+genes <- genes[!is.na(genes$padj),]
+genes <- genes[genes$padj < 0.05,]
+genes <- genes$X
+idx <- match(genes,gene_id$ensembl_gene_id)
+entrez <- gene_id$entrezgene_id[idx]
+entrez <- entrez[!is.na(entrez)]
+entrez <- as.character(entrez)
+
+ggo <- groupGO(gene = entrez,
+               OrgDb = org.Dr.eg.db,
+               ont = "CC",
+               #level = 3,
+               readable = T)
+
+kk <- enrichGO(gene = entrez,
+               OrgDb = org.Dr.eg.db,
+               pAdjustMethod = "BH",
+               ont="BP",
+               readable = T)
+
+kk <- enrichGO(gene = entrez,
+               OrgDb = org.Dr.eg.db,
+               pAdjustMethod = "BH",
+               ont="MF",
+               readable = T)
+
+kk <- enrichGO(gene = entrez,
+               OrgDb = org.Dr.eg.db,
+               pAdjustMethod = "BH",
+               ont="CC",
+               readable = T)
+
+head(kk)
+barplot(kk)
+goplot(kk)
+
+# -- KEEG
+kk <- enrichKEGG(gene = entrez,
+                 organism = "dre",
+                 pvalueCutoff = 0.05)
+
+head(kk)
+kk$ID
+
+browseKEGG(kk, 'dre00330')
